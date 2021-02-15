634d588f1394d094b04150aef4c22b878b355d45
Shirley-Dongxx <58315188+Shirley-Dongxx@users.noreply.github.com>
2021-02-15 02:19:21 -0500

    Tried oscilloscope with data from Arduino

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import serial
 import time
+import matplotlib.pyplot as plt
+
 ser = serial.Serial('/dev/cu.usbmodem141101', 9800, timeout=1)
 cond = 0
-def led_on_off():
+'''def led_on_off():
     data = ser.readline()[:-2]  # the last bit gets rid of the new-line chars
     if data:
         print(data)
@@ -32,4 +34,35 @@ def led_on_off():
 time.sleep(2) # wait for the serial connection to initialize
 
 led_on_off()
+ser.close()'''
+
+def isfloat(value):
+  try:
+    float(value)
+    return True
+  except ValueError:
+    return False
+
+# Read and record the data
+data =[]                       # empty list to store the data
+for i in range(20):
+    b = ser.readline()         # read a byte string
+    string_n = b.decode()  # decode byte string into Unicode
+    string = string_n.rstrip() # remove \n and \r
+    print("curr: ",string)
+    if (isfloat(string)):
+        flt = float(string)
+    else:
+        flt = 0
+    #flt = float(string)        # convert string to float
+    #print(flt)
+    data.append(flt)           # add to the end of data list
+    time.sleep(0.1)            # wait (sleep) 0.1 seconds
+
 ser.close()
+
+plt.plot(data)
+plt.xlabel('Time (seconds)')
+plt.ylabel('Potentiometer Reading')
+plt.title('Potentiometer Reading vs. Time')
+plt.show()
